dfdacfe003bd36dfa07167ec659838894cc0388b
MADDULA BALA TULASI RAM <113907928+balatulasirammaddula@users.noreply.github.com>
2022-11-03 16:37:55 +0530

    def palindrome(n):     s=str(n)     if s==s[::-1]:         return True     return False n=int(input()) if palindrome(n)==True:     print('True') else:     print('False')

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,11 @@
+def palindrome(n):
+    s=str(n)
+    if s==s[::-1]:
+        return True
+    return False
 n=int(input())
-rev=0
-t=n
-while n>0: 
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-if rev==t:
-  print('True')
+if palindrome(n)==True:
+    print('True')
 else:
     print('False')
+                
