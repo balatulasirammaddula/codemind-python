158f01dc008182280a1bcc86eb4f8217cb12fc17
MADDULA BALA TULASI RAM <113907928+balatulasirammaddula@users.noreply.github.com>
2023-07-10 12:11:28 +0530

    def prnt(n):     temp=n     cnt=0     s=str(temp)     l=len(s)     while n>0:         r=n%10         if r==0:             break         elif temp%r==0:             cnt+=1         n=n//10     if cnt==l:         return True     else:         return False a=int(input()) b=int(input()) for i in range(a,b+1):     if prnt(i)==True:         print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,16 +1,21 @@
+def prnt(n):
+    temp=n
+    cnt=0
+    s=str(temp)
+    l=len(s)
+    while n>0:
+        r=n%10
+        if r==0:
+            break
+        elif temp%r==0:
+            cnt+=1
+        n=n//10
+    if cnt==l:
+        return True
+    else:
+        return False
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    temp=i
-    s=str(temp)
-    s=len(s)
-    c=0
-    while i>0:
-        r=i%10
-        if r==0:
-            pass
-        elif temp%r==0:
-            c+=1
-        i=i//10
-    if s==c:
-        print(temp,end=' ')
+    if prnt(i)==True:
+        print(i,end=' ')
